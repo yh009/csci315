610f0ed7eec8ce276cc8d2be91d4e599b6d4b70d
Yuxuan Huang <yh009@bucknell.edu>
2015-09-01 14:31:03 -0400

    Pre-lab 1.3 completed

--- Labs/Lab1/myproc.c
+++ Labs/Lab1/myproc.c
@@ -1,38 +1,39 @@
-/*Yuxuan H/Volumes/linux-yh009.$/csci315/Labs/Lab1/myproc.cuang
+/*Yuxuan Huang
   csci315 prelab1 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(){
-    int p;
-    int c1;
-    int c2;
-    int p_next=1;
-    int c1_next=1;
-    int c2_next=1;
+  pid_t child1;
+  pid_t child2;
+  int p=1;
+  int c1=1;
+  int c2=1;
+
+  child1=fork();
+  if(child1!=0){
+    child2=fork();
+      }
     
     while (1){
-        p = p_next;
-        p_next++;
-        
-        
-        printf("parent:%i\n",p);
-        while(1){
-            c1 = c1_next;
-            c1_next++;
-            
-            printf("child1:%i\n",c1);
-            break;
-        }
-        while(1){
-            c2 = c2_next;
-            c2_next++;
-            
-            printf("child2:%i\n",c2);
-            break;
-        }
+      if (child1!=0 && child2!=0){
+	printf("parent:%i\n",p);
+        p++;
+      }
+      else{
+	if(child1==0){
+	  printf("child1:%i\n",c1);
+	  c1++;
+	}
+	else if(child2==0){
+	    printf("child2:%i\n",c2);
+	    c2++;
+	  }
+      }
     }
-}
+}     
+
 
