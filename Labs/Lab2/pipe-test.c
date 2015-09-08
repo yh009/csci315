644325bd83976b453ef3627d4410dbc45ea7b507
Yuxuan Huang <yh009@bucknell.edu>
2015-09-07 23:23:12 -0400

    Pre-lab 2.2 completed

---
+++ Labs/Lab2/pipe-test.c
@@ -0,0 +1,56 @@
+//Yuxuan Huang
+//csci315 prelab2
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+int main(void){
+    char write_msg[BUFFER_SIZE] = "Greetings\n";
+    char read_msg[BUFFER_SIZE];
+    int fd[2];
+    pid_t pid;
+    
+    /*create the pipe*/
+    if (pipe(fd) == -1){
+        fprintf(stderr, "Pipe failed");
+        return 1;
+        
+    }
+    /*fork a child*/
+    pid = fork();
+    
+    if(pid < 0){
+        fprintf(stderr, "Fork Failed");
+        return 1;
+        
+    }
+    if (pid > 0){/* parent process*/
+        /*close the unused end of the pipe*/
+        close(fd[READ_END]);
+        
+        /*write to the pipe*/
+        write(fd[WRITE_END],write_msg,strlen(write_msg)+1);
+        
+        /*close the write end of the pipe*/
+        close(fd[WRITE_END]);
+        
+    }
+    else{/*child process*/
+        /*close the unused end of the pipe*/
+        close(fd[WRITE_END]);
+        
+        /*read from the pipe*/
+        read(fd[READ_END],read_msg,BUFFER_SIZE);
+        printf("read %s",read_msg);
+        
+        /*close the write end of the pipe*/
+        close(fd[READ_END]);
+    }
+    return 0;
+}
